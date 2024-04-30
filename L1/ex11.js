b6f44a4db96c2106b63b2c5a9efa959b02ab9d38
maisabb <168444983+maisabb@users.noreply.github.com>
2024-04-30 12:13:23 -0300

    Update ex11.js

--- L1/ex11.js
+++ L1/ex11.js
@@ -3,8 +3,13 @@ const prompt = require ("prompt-sync")();
 let num;
 
 for (let contador = 0; ; contador++){
-    num = prompt ();
+    num = prompt ("Digite um  número: ");
     if (num <= 0){
         break;
+    }
+    if (num % 2 == 0){
+        console.log ("O número é par");
+    }else if (num % 2 != 0 && num % 3 == 0){
+        console.log ("O número é ímpar");
     }
 }
