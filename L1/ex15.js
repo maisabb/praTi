1aac28104e65f3bd144aa6bfc33d24760ced380b
maisabb <168444983+maisabb@users.noreply.github.com>
2024-04-30 12:18:19 -0300

    Update ex15.js

--- L1/ex15.js
+++ L1/ex15.js
@@ -3,12 +3,12 @@ const prompt = require ("prompt-sync")();
 let num, peso, conta = 0, contador = 0;
 
 
-while ((num = parseFloat(prompt())) != 0){
-    peso = parseInt (prompt());
+while ((num = parseFloat(prompt("Digite um número decimal: "))) != 0){
+    peso = parseInt (prompt("Digite o peso dele: "));
     conta += peso * num;
     contador += peso;
 }
 
 let mediaPonderada = conta/contador;
 
-console.log (mediaPonderada.toFixed(2));
+console.log ("A média ponderada é igual a",mediaPonderada.toFixed(2));
