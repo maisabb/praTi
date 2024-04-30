767af67330e82e4409653daa75cdbe3f60466fae
maisabb <168444983+maisabb@users.noreply.github.com>
2024-04-30 12:19:29 -0300

    Update ex14.js

--- L1/ex14.js
+++ L1/ex14.js
@@ -3,11 +3,11 @@ const prompt = require ("prompt-sync")();
 let num;
 let conta = 0, contador = 0;
 
-while ((num = parseFloat(prompt())) != 0){
+while ((num = parseFloat(prompt("Digite um número decimal: "))) != 0){
     conta += num;
     contador++;
 }
 
 let media = conta/contador;
 
-console.log (media.toFixed(1));
+console.log ("A média aritmética é igual a",media.toFixed(1));
