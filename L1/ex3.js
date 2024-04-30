a7a0f173a0c875cb31ae830111cb5df34efa91f1
maisabb <168444983+maisabb@users.noreply.github.com>
2024-04-30 12:23:17 -0300

    Update ex3.js

--- L1/ex3.js
+++ L1/ex3.js
@@ -1,9 +1,9 @@
 const prompt = require ("prompt-sync")();
 
-let num1 = parseInt (prompt ());
-let num2 = parseInt (prompt ());
-let num3 = parseInt (prompt ());
-let num4 = parseInt (prompt ());
+let num1 = parseInt (prompt ("Primeiro número: "));
+let num2 = parseInt (prompt ("Segundo número: "));
+let num3 = parseInt (prompt ("Terceiro número: "));
+let num4 = parseInt (prompt ("Quarto número: "));
 
 num4 = num1 + num2 + num3;
 num1 += 25;
