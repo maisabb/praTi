998158d1c9b65533f9ef40932af0011bab9c37d8
maisabb <168444983+maisabb@users.noreply.github.com>
2024-06-25 11:39:22 -0300

    Update 34.js

--- L2/34.js
+++ L2/34.js
@@ -1,8 +1,8 @@
 let matriz = [], diagonalP = [];
 
-for (let i = 0; i < 3; i++){
+for (let i = 0; i < 50; i++){
     matriz[i] = [];
-    for (let j = 0; j < 3; j++){
+    for (let j = 0; j < 50; j++){
         matriz[i][j] = Math.floor((Math.random() * 20) + 1);
         if (i == j){
             diagonalP[i] = matriz[i][j];
@@ -12,8 +12,8 @@ for (let i = 0; i < 3; i++){
 
 console.log(matriz);
 
-for (let i = 0; i < 3; i++){
-    for (let j = 0; j < 3; j++){
+for (let i = 0; i < 50; i++){
+    for (let j = 0; j < 50; j++){
         matriz[i][j] *= diagonalP[i];
     }
 }
