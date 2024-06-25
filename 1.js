ebe7c184d3aa8e1bb01a408e61a3110137d78b54
maisabb <168444983+maisabb@users.noreply.github.com>
2024-06-25 11:24:57 -0300

    Delete 1.js

--- 1.js
+++
@@ -1,9 +0,0 @@
-const prompt = require('prompt-sync')();
-
-let qntCigarros = prompt ("Quantos cigarros vc usa por dia? ");
-
-let anos = prompt ("Há qnt tempo vc fuma? ");
-
-let tempoPerdido = (qntCigarros * 10) * 365 * anos;
-
-console.log ("Você já perdeu " + tempoPerdido + " de vida");
